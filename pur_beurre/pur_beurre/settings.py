6ec43d33ef42b6bf0b889b8ebbe64e4d2dcb467f
Fabe59 <pringuetfabrice@gmail.com>
2020-06-15 15:36:26 +0200

    reset password function updated

--- pur_beurre/pur_beurre/settings.py
+++ pur_beurre/pur_beurre/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['.herokuapps.com', 'localhost', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'users.apps.UsersConfig',
     'food.apps.FoodConfig',
     'crispy_forms',
 ]
--- pur_beurre/pur_beurre/urls.py
+++ pur_beurre/pur_beurre/urls.py
@@ -18,8 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("users/", include("django.contrib.auth.urls")),
     path('', include('food.urls')),
     path('users/', include('users.urls')),
+    path("users/", include("django.contrib.auth.urls")),
 
 ]
